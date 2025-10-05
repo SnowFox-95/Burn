1e296759e9c656e7aa5e978b5b686fbf7b455473
SnowFox-95 <321mts95@gmail.com>
2025-10-05 23:31:59 +0300

    add blocks project

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+// Основной файл JavaScript
+// В данном проекте нет интерактивных элементов, но структура готова для расширения
+
+console.log('Burn project loaded');
+
+// Импортируем модули (пока нет, но структура готова)
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,14 +1,12 @@
-function mobileNav() {
-	// Mobile nav button
-	const navBtn = document.querySelector('.mobile-nav-btn');
-	const nav = document.querySelector('.mobile-nav');
-	const menuIcon = document.querySelector('.nav-icon');
-
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
-		document.body.classList.toggle('no-scroll');
-	};
+// Мобильная навигация (заготовка для будущего функционала)
+export default function mobileNav() {
+    console.log('Mobile navigation module');
+    
+    // Здесь будет логика мобильного меню
+    // const menuToggle = document.querySelector('.menu-toggle');
+    // const mobileMenu = document.querySelector('.mobile-menu');
+    
+    // menuToggle.addEventListener('click', () => {
+    //     mobileMenu.classList.toggle('active');
+    // });
 }
-
-export default mobileNav;
